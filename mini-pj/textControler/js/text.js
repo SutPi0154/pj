92c6fd43f3c399bad68df291eef949b9cdadbba4
SutPi0154 <sutpi070@gmail.com>
2023-01-30 17:47:33 +0630

    done pj

--- mini-pj/textControler/js/text.js
+++ mini-pj/textControler/js/text.js
@@ -10,6 +10,39 @@ const count = document.querySelector("#count");
 const color = document.querySelector("#color");
 const fontSize = document.querySelector("#fontSize");
 const fontFamily = document.querySelector("#fontFamily");
+const speech = document.querySelector("#speech");
+
+const synth = window.speechSynthesis;
+
+const speak = (text) => {
+  const utterance = new SpeechSynthesisUtterance(text);
+  synth.speak(utterance);
+  utterance.text = text;
+
+  utterance.addEventListener("start", () => {
+    speech.classList.add("active");
+  });
+  utterance.addEventListener("end", () => {
+    speech.classList.remove("active");
+  });
+};
+const listen = () => {
+  const recognition = new webkitSpeechRecognition();
+
+  recognition.lang = "en-US";
+  // recognition.interimResults = false;
+  // recognition.maxAlternatives = 1;
+
+  recognition.start();
+  recognition.onresult = (event) => {
+    const text = event.results[0][0].transcript;
+    console.log(text);
+  };
+
+  // const listen = () => {};
+
+  // listen();
+};
 
 fonts.forEach((font) => {
   fontFamily.append(new Option(font, font));
@@ -32,6 +65,10 @@ fontSize.addEventListener("change", (event) => {
 });
 
 fontFamily.addEventListener("change", (event) => {
-//   console.log(event.target.value);   
+  //   console.log(event.target.value);
   output.style.fontFamily = event.target.value;
+});
+
+speech.addEventListener("click", () => {
+  speak(text.value);
 });
